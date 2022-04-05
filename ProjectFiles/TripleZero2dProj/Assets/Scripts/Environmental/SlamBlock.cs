56b55052f59151288b27e2c486851d1da2d915c8
Dan Sohler <daniel.sohkler73@gmail.com>
2022-04-05 12:24:11 +1000

    #9 Slam Block added
    There is a minor issue with it always being reset to 0, issue with the animator, double check to enable it to be modular.

---
+++ ProjectFiles/TripleZero2dProj/Assets/Scripts/Environmental/SlamBlock.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlamBlock : MonoBehaviour
+{
+    Animator slamAnim;
+
+    [SerializeField] float waitTime;
+    [SerializeField] [Range(0, 1)] float animationOffset;
+
+    private void Start()
+    {
+        slamAnim = GetComponent<Animator>();
+        slamAnim.SetFloat("WaitTime", 1 / waitTime);
+        slamAnim.Play("WaitTime", -1, animationOffset);
+    }
+
+}
