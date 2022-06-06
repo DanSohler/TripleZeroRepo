7798bf3d14246f173404186ef2d8640a9aca71c1
Daniel Sohler <daniel.sohler73@gmail.com>
2022-06-06 16:30:50 +1000

    Not final, added main menu, added button to swap scenes

--- ProjectFiles/TripleZero2dProj/Assets/Scripts/SceneSwitch.cs
+++ ProjectFiles/TripleZero2dProj/Assets/Scripts/SceneSwitch.cs
@@ -8,8 +8,23 @@ public class SceneSwitch : MonoBehaviour
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other)
     {
+        SceneManager.LoadScene(2);
+    }
+
+
+    public void PlayLevel()
+    {
         SceneManager.LoadScene(1);
     }
 
-  
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
