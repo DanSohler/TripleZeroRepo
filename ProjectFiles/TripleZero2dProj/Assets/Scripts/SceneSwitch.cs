f8bf635da6f3829240289250fa64671af2ef2d85
Daniel Sohler <daniel.sohler73@gmail.com>
2022-06-06 16:46:26 +1000

    Added win condition and better scene transition

--- ProjectFiles/TripleZero2dProj/Assets/Scripts/SceneSwitch.cs
+++ ProjectFiles/TripleZero2dProj/Assets/Scripts/SceneSwitch.cs
@@ -5,10 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
+    public bool isDeathBox = false;
+    public GameObject winMenu;
+    public GameObject button1;
+    public GameObject button2;
+
+
+    private void Start()
+    {
+        winMenu.SetActive(false);
+        button1.SetActive(false);
+        button2.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other)
     {
-        SceneManager.LoadScene(2);
+        if (isDeathBox == true)
+        {
+            SceneManager.LoadScene(2);
+        }
+        else if (!isDeathBox)
+        {
+            winMenu.SetActive(true);
+            button1.SetActive(true);
+            button2.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
     }
 
 
